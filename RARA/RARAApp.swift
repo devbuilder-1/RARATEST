ff8601dc3ac406b37535a0833ae00a4eaf07e764
Prithiv Dev <dev.prithiv@yahoo.com>
2021-03-15 15:41:56 -0400

    Initial Commit

---
+++ RARA/RARAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RARAApp.swift
+//  RARA
+//
+//  Created by Prithiv Dev on 15/03/21.
+//
+
+import SwiftUI
+
+@main
+struct RARAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
